7ccc28e7caef9e3f620361cfd2a5e9e1265baa61
Holger Veltrup <veltrup@sitepark.com>
2024-03-14 09:45:22 +0100

    feat: work in process

--- src/Loader/ServerVarResourceBaseLocator.php
+++ src/Loader/ServerVarResourceBaseLocator.php
@@ -5,27 +5,43 @@ declare(strict_types=1);
 namespace Atoolo\Resource\Loader;
 
 use Atoolo\Resource\ResourceBaseLocator;
+use RuntimeException;
 
 class ServerVarResourceBaseLocator implements ResourceBaseLocator
 {
+    private ?string $resourceBase = null;
+
     public function __construct(
-        private readonly string $variableName,
-        private readonly ?string $subDirectory = null
+        private readonly string $variableName
     ) {
     }
 
     public function locate(): string
     {
+        if ($this->resourceBase !== null) {
+            return $this->resourceBase;
+        }
+
         if (empty($_SERVER[$this->variableName])) {
             throw new \RuntimeException(
                 'missing server variable ' . $this->variableName
             );
         }
 
-        if ($this->subDirectory === null) {
+        $resourceLayoutResourceBase = $_SERVER[$this->variableName] . '/object';
+        if (is_dir($resourceLayoutResourceBase)) {
+            $this->resourceBase = $resourceLayoutResourceBase;
+            return $resourceLayoutResourceBase;
+        }
+
+        if (is_dir($_SERVER[$this->variableName])) {
+            $this->resourceBase = $_SERVER[$this->variableName];
             return $_SERVER[$this->variableName];
         }
 
-        return $_SERVER[$this->variableName] . '/' . $this->subDirectory;
+        throw new RuntimeException(
+            "Resource root directory not found: " .
+            $_SERVER[$this->variableName]
+        );
     }
 }
